36c0642adc339c8fa9bd8a96b609e78f81f506c9
timigen <timigen@gmail.com>
2019-10-18 18:38:02 -0400

    clean up

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    minimize: false
+module.exports = (env, argv) => {
+  const isDev = argv.mode === 'development';
+
+  let config = {
+    mode: argv.mode,
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+      minimize: isDev ? false: true
+    }
   }
+
+  return config;
 };
