76c3e7a56dd55fdacc1aa856a36a80d64f2bece6
timigen <timigen@gmail.com>
2019-11-20 22:43:42 -0500

    tslint

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -6,11 +6,11 @@ export class Numbers {
         return b;
       }
     }
-  
+
     public static getRandomInt(min: number, max: number): number {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-  
+
     public static roundBy(target: number, rounder: number): number {
       return Math.floor(target / rounder) * rounder;
     }
