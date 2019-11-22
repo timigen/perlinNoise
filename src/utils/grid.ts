56b9c9bba78c5b0c56742203f4b277702405149a
timigen <timigen@gmail.com>
2019-11-21 20:35:58 -0500

    running prettier

--- src/utils/grid.ts
+++ src/utils/grid.ts
@@ -1,40 +1,37 @@
 import { Numbers } from './numbers';
 
 export class Grid {
-    private size: number;
+  private size: number;
 
-    constructor(size: number) {
-        this.size = size;
+  constructor(size: number) {
+    this.size = size;
+  }
+
+  generateRandomArray(minimum: number, maximum: number) {
+    let arr: number[] = [];
+
+    for (let row = 0; row < this.size; row++) {
+      let num = Numbers.getRandomInt(minimum, maximum);
+
+      arr.push(num);
     }
 
-    generateRandomArray(minimum:number, maximum: number) {
-        let arr: number[] = [];
-        
-        for( let row = 0; row < this.size; row++ ) {
-            let num = Numbers.getRandomInt(minimum, maximum);
+    return arr;
+  }
 
-            arr.push(num);
-        }
+  generateRandom(minimum: number, maximum: number): number[][] {
+    let grid: number[][] = [];
 
-        return arr;
+    for (let row = 0; row < this.size; row++) {
+      let newRow = [];
+      for (let col = 0; col < this.size; col++) {
+        let num = Numbers.getRandomInt(minimum, maximum);
+
+        newRow.push(num);
+      }
+      grid.push(newRow);
     }
-    
-    generateRandom(minimum:number, maximum: number): number[][] {
-    
-        let grid: number[][] = [];
-        
-        
-        for( let row = 0; row < this.size; row++ ) {
-            let newRow = [];
-            for( let col = 0; col < this.size; col++) {
-    
-                let num = Numbers.getRandomInt(minimum, maximum);
 
-                newRow.push(num);
-            }
-            grid.push(newRow);
-        }
-    
-        return grid;
-    }
+    return grid;
+  }
 }
--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,9 +1,9 @@
 export class Numbers {
-    public static getRandomInt(min: number, max: number): number {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-  
-    public static roundBy(target: number, rounder: number): number {
-      return Math.floor(target / rounder) * rounder;
-    }
+  public static getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+  public static roundBy(target: number, rounder: number): number {
+    return Math.floor(target / rounder) * rounder;
+  }
+}
