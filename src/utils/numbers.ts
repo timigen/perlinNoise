0dfe3062cb8ab781b7c0506aefd15ddb13c890bc
timigen <timigen@gmail.com>
2019-11-21 20:18:08 -0500

    install prettier

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,12 +1,4 @@
 export class Numbers {
-    public static largest(a: number, b: number): number {
-      if (a > b) {
-        return a;
-      } else {
-        return b;
-      }
-    }
-  
     public static getRandomInt(min: number, max: number): number {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
