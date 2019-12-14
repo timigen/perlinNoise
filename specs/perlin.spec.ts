2de9294deb9b9698088d612205f59e0eb915437d
timigen <timigen@gmail.com>
2019-12-14 14:17:20 -0500

    update specs

--- specs/perlin.spec.ts
+++ specs/perlin.spec.ts
@@ -2,23 +2,37 @@ import { expect } from 'chai';
 
 import { Perlin } from '../src/perlin';
 
-const cols = 10;
-const rows = 10;
+const cols = 17;
+const rows = 15;
 
 describe('Perlin', () => {
-  describe('constructor', () => {
-    it(`new Perlin(${rows}, ${cols}) does not throw exception`, () => {
+  describe(`new Perlin(${rows}, ${cols})`, () => {
+    it(`does not throw exception`, () => {
       expect(() => {
         new Perlin(rows, cols);
       }).not.to.throw(Error);
     });
   });
 
-  describe('constructor', () => {
-    it(`new Perlin(${rows}, ${cols}).generate2D() does not throw exception`, () => {
+  describe(`new Perlin(${rows}, ${cols}).generate2D()`, () => {
+    it(`does not throw exception`, () => {
       expect(() => {
         new Perlin(rows, cols).generate2D();
       }).not.to.throw(Error);
+    });
+
+    const grid = new Perlin(rows, cols).generate2D();
+
+    it(`not null`, () => {
+      expect(grid.length).not.equal(null);
+    });
+
+    it(`rows to equal ${rows}`, () => {
+      expect(grid.length).to.equal(rows);
+    });
+
+    it(`cols to equal ${cols}`, () => {
+      expect(grid[0].length).to.equal(cols);
     });
   });
 });
